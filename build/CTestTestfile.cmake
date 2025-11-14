077a83c7b6dcbc1c74b27c910e5d827ea1b4175f
mcst2014 <mcst2014@outlook.com>
2025-11-14 11:43:23 -0600

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 6 Robots/robots
+# Build directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 6 Robots/robots/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
